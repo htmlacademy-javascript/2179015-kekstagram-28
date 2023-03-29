52e1c75bb1ad85300c0e2e344ea669c1239d4c13
Keks <keks@htmlacademy.ru>
2023-03-29 10:40:58 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/big-picture.js
@@ -0,0 +1,34 @@
+import { comment } from './data.js';
+const bigPicture = document.querySelector('.big-picture');
+const commentsContainer = bigPicture.querySelector('.social__comments');
+const commentTemplate = commentsContainer.querySelector('.social__comment');
+
+const renderComment = ({ avatar, name, message }) => {
+  const commentItem = commentTemplate.cloneNode(true);
+  commentItem.querySelector('.social__picture').src = avatar;
+  commentItem.querySelector('.social__picture').alt = name;
+  commentItem.querySelector('.social__text').textContent = message;
+
+  return commentItem;
+};
+
+const createCommentList = (comments) => {
+  const commentsListFragment = document.createDocumentFragment();
+  comments.forEach((commentItem) => {
+    commentsListFragment.append(renderComment(commentItem));
+  });
+
+  commentsContainer.append(commentsListFragment);
+};
+
+const createBigPicture = (pictureObject) => {
+  bigPicture.querySelector('.big-picture__img').querySelector('img').src = pictureObject.url;
+  //bigPicture.querySelector('.big-picture__img').querySelector('img').alt = pictureObject.description;
+  bigPicture.querySelector('.social__caption').textContent = pictureObject.description;
+  bigPicture.querySelector('.likes-count').textContent = pictureObject.likes;
+  bigPicture.querySelector('.comments-count').textContent = pictureObject.comments.length;
+  commentsContainer.innerHTML = '';
+  createCommentList(comment);
+};
+
+export { createBigPicture };
---
+++ 6/js/main.js
@@ -0,0 +1,6 @@
+import './gallery.js';
+import { data } from './data.js';
+import { renderThumbnails } from './thumbnail.js';
+
+renderThumbnails(data);
+
