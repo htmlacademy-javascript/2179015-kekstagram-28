14ede761d9c790d0f206817908221bbb3b30757b
Nataliya Ilchenko <sixtygirl@list.ru>
2023-04-11 22:44:08 +0300

    Защита

--- js/util.js
+++ js/util.js
@@ -13,7 +13,7 @@ const showAlert = (message) => {
   }, ALERT_SHOW_TIME);
 };
 
-const debounce = (callback, timeoutDelay) => {
+const debounce = (callback, timeoutDelay = 500) => {
   let timeoutId;
   return (...rest) => {
     clearTimeout(timeoutId);
